9d229b1e4bb0501d4f11a8c9c80cb46160b69781
jjgweb951101@gmail.com <jone1542%%>
2021-08-20 17:41:07 +0900

    8/20 수업

---
+++ WpfHmiSolution/SyntaxWpfApp/ThirdWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SyntaxWpfApp
+{
+    /// <summary>
+    /// ThirdWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ThirdWindow : Window
+    {
+        public ThirdWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //txt.Text = "Meeting";
+            //txt.FontSize = 10 ;
+            //txt.Foreground = new SolidColorBrush(Colors.Black);
+            
+            // Car
+            Car c = new Car();
+            c.Speed = 300;
+            c.Color = Colors.White;
+            c.Driver = null;
+
+            this.DataContext = c;
+            //txt.Text = c.Speed.ToString();
+        }
+        
+    }
+}
---
+++ WpfHmiSolution/SyntaxWpfApp/TwiceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SyntaxWpfApp
+{
+    public class TwiceConverter : IValueConverter
+    {
+        // ONEWAY 구현
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.Parse(value.ToString()) * 2 ;
+        }
+
+        // TWOWAY 구현
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
